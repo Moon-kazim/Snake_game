d64411c866b08f701d627bf5470ed360643e9fab
Moon-kazim <kazim.shaikh0@gmail.com>
2025-01-10 03:29:06 +0530

    adding file in our game to remember the last h_score

--- scoreboard.py
+++ scoreboard.py
@@ -2,12 +2,13 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
-
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        # self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(x=0,y=270)
@@ -25,6 +26,8 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
